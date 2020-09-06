bd29519ab56ab5e9d46b13e51f31ceebb9535b1a
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-06 12:55:17 -0400

    Write tests for Dock#rent. Write code to pass tests.

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -17,5 +17,19 @@ describe Dock do
     it 'has a max rental time' do
       expect(subject.max_rental_time).to eql 3
     end
+
+    it 'has an empty rental_log' do
+      expect(subject.rental_log).to eql({})
+    end
+  end
+
+  describe '#rent' do
+    it 'add rented boats to rental_log' do
+      kayak = Boat.new(:kayak, 20)
+      patrick = Renter.new('Patrick Star', '4242424242424242')
+      subject.rent(kayak, patrick)
+
+      expect(subject.rental_log).to eql({ kayak => patrick })
+    end
   end
 end
