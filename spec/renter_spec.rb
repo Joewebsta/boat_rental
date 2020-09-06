cf87196b46a443bbde0722e2d525669a9832d70e
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-06 12:08:35 -0400

    Write Renter#init tests. Write code to pass tests.

---
+++ spec/renter_spec.rb
@@ -0,0 +1,20 @@
+require './lib/boat'
+require './lib/renter'
+
+describe Renter do
+  subject { Renter.new('Joe Webster', '424224242424') }
+
+  describe '#init' do
+    it 'is an instance of Renter' do
+      expect(subject).to be_a Renter
+    end
+
+    it 'has a name' do
+      expect(subject.name).to eql 'Joe Webster'
+    end
+
+    it 'has a credit card number' do
+      expect(subject.credit_card_number).to eql '424224242424'
+    end
+  end
+end
